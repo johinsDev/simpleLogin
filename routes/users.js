e1eaee5a35ba89810f1a1bd9ee8f3c581ee5ddab
Johan Villamil <johan.villamil@mocionsoft.com>
2017-05-11 16:34:56 -0500

    Login view

--- routes/users.js
+++ routes/users.js
@@ -2,8 +2,11 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/sign_in', function(req, res, next) {
+  res.render('users/sing_in')
 });
 
+router.get('/sign_up', function(req, res, next) {
+    res.render('users/sing_up')
+});
 module.exports = router;
